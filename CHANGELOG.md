f2b4436fa992d3777baecf86bb83259c46441471
Hadrien Lejard <hadrien.lejard@gmail.com>
2017-05-27 12:33:36 +0200

    doc

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.0.3
+
+- Documentation
+
+# 0.0.2
+
+- Initial version for Android
--- lib/flutter_webview_plugin.dart
+++ lib/flutter_webview_plugin.dart
@@ -2,33 +2,22 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+const _kChannel = 'flutter_webview_plugin';
+
+/// Singleton Class that communicate with a fullscreen Webview Instance
+/// Have to be instanciate after `runApp` called.
 class FlutterWebviewPlugin {
+
+  final MethodChannel _channel = const MethodChannel(_kChannel);
+  StreamController<Null> _onDestroy = new StreamController.broadcast();
+  StreamController<Null> _onBackPressed = new StreamController.broadcast();
+
   static FlutterWebviewPlugin _instance;
   FlutterWebviewPlugin._() {
     _init();
   }
+
   factory FlutterWebviewPlugin() => _instance ??= new FlutterWebviewPlugin._();
-
-  StreamController<Null> _onDestroy = new StreamController.broadcast();
-  Stream<Null> get onDestroy => _onDestroy.stream;
-
-  StreamController<Null> _onBackPressed = new StreamController.broadcast();
-  Stream<Null> get onBackPressed => _onBackPressed.stream;
-
-  final MethodChannel _channel = const MethodChannel('flutter_webview_plugin');
-
-  Future<Null> launch(String url,
-          {bool withJavascript: true,
-          bool clearCache: false,
-          bool clearCookies: false}) =>
-      _channel.invokeMethod('launch', {
-        "url": url,
-        "withJavascript": withJavascript,
-        "clearCache": clearCache,
-        "clearCookies": clearCookies
-      });
-
-  Future<Null> close() => _channel.invokeMethod("close");
 
   _init() {
     _channel.setMethodCallHandler(_handleMessages);
@@ -44,4 +33,33 @@ class FlutterWebviewPlugin {
         break;
     }
   }
+
+  //////////////////////
+
+  /// Listening the OnDestroy LifeCycle Event for Android
+  ///
+  Stream<Null> get onDestroy => _onDestroy.stream;
+
+  /// Listening the onBackPressed Event for Android
+  ///
+  Stream<Null> get onBackPressed => _onBackPressed.stream;
+
+  /// Start the Webview with [url]
+  /// - [withJavascript] enable Javascript or not for the Webview
+  /// - [clearCache] clear the cache of the Webview
+  /// - clearCookies] clear all cookies of the Webview
+  Future<Null> launch(String url,
+          {bool withJavascript: true,
+          bool clearCache: false,
+          bool clearCookies: false}) =>
+      _channel.invokeMethod('launch', {
+        "url": url,
+        "withJavascript": withJavascript,
+        "clearCache": clearCache,
+        "clearCookies": clearCookies
+      });
+
+  /// Close the Webview
+  /// Will trigger the [onDestroy] event
+  Future<Null> close() => _channel.invokeMethod("close");
 }
