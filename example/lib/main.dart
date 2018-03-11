642b3c1f661cc609ae01896efb0f0fbb3ede2e5a
Lejard Hadrien <hadrien.lejard@gmail.com>
2018-03-11 12:03:02 +0100

    support zoom

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 const kAndroidUserAgent =
     "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36";
 
-String selectedUrl = "https://github.com/dart-flitter/flutter_webview_plugin";
+String selectedUrl = "https://flutter.io";
 
 void main() {
   runApp(new MyApp());
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
               appBar: new AppBar(
                 title: new Text("Widget webview"),
               ),
+              withZoom: true,
             )
       },
     );
