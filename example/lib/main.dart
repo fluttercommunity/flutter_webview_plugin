eed3a3bb4de8c132d96b343944cbd3eb11d2cf29
rin.lv <rinlv@sigma-solutions.eu>
2019-09-16 11:50:47 +0700

    fixed by comment https://github.com/fluttercommunity/flutter_webview_plugin/pull/523

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 const kAndroidUserAgent =
     'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
 
-String selectedUrl = 'https://damp-coast-35782.herokuapp.com';
+String selectedUrl = 'https://flutter.io';
 
 // ignore: prefer_collection_literals
 final Set<JavascriptChannel> jsChannels = [
