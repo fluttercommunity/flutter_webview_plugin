41adc79b8e767a40c31a167b29eaf0175043bbcb
ivanasen <ivanasenchakarov@gmail.com>
2019-09-27 23:57:58 +0300

    Create WebviewRect

--- lib/src/webview_scaffold.dart
+++ lib/src/webview_scaffold.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_webview_plugin/src/javascript_channel.dart';
+import 'package:flutter_webview_plugin/src/webview_rect.dart';
 
-import 'base.dart';
-
-class WebviewScaffold extends StatefulWidget {
+class WebviewScaffold extends StatelessWidget {
   const WebviewScaffold({
     Key key,
     this.appBar,
@@ -72,182 +68,37 @@ class WebviewScaffold extends StatefulWidget {
   final bool debuggingEnabled;
 
   @override
-  _WebviewScaffoldState createState() => _WebviewScaffoldState();
-}
-
-class _WebviewScaffoldState extends State<WebviewScaffold> {
-  final webviewReference = FlutterWebviewPlugin();
-  Rect _rect;
-  Timer _resizeTimer;
-  StreamSubscription<WebViewStateChanged> _onStateChanged;
-
-  var _onBack;
-
-  @override
-  void initState() {
-    super.initState();
-    webviewReference.close();
-
-    _onBack = webviewReference.onBack.listen((_) async {
-      if (!mounted) {
-        return;
-      }
-
-      // The willPop/pop pair here is equivalent to Navigator.maybePop(),
-      // which is what's called from the flutter back button handler.
-      final pop = await _topMostRoute.willPop();
-      if (pop == RoutePopDisposition.pop) {
-        // Close the webview if it's on the route at the top of the stack.
-        final isOnTopMostRoute = _topMostRoute == ModalRoute.of(context);
-        if (isOnTopMostRoute) {
-          webviewReference.close();
-        }
-        Navigator.pop(context);
-      }
-    });
-
-    if (widget.hidden) {
-      _onStateChanged =
-          webviewReference.onStateChanged.listen((WebViewStateChanged state) {
-        if (state.type == WebViewState.finishLoad) {
-          webviewReference.show();
-        }
-      });
-    }
-  }
-
-  /// Equivalent to [Navigator.of(context)._history.last].
-  Route<dynamic> get _topMostRoute {
-    var topMost;
-    Navigator.popUntil(context, (route) {
-      topMost = route;
-      return true;
-    });
-    return topMost;
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _onBack?.cancel();
-    _resizeTimer?.cancel();
-    webviewReference.close();
-    if (widget.hidden) {
-      _onStateChanged.cancel();
-    }
-    webviewReference.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: widget.appBar,
-      resizeToAvoidBottomInset: widget.resizeToAvoidBottomInset,
-      persistentFooterButtons: widget.persistentFooterButtons,
-      bottomNavigationBar: widget.bottomNavigationBar,
-      body: _WebviewPlaceholder(
-        onRectChanged: (Rect value) {
-          if (_rect == null) {
-            _rect = value;
-            webviewReference.launch(
-              widget.url,
-              headers: widget.headers,
-              javascriptChannels: widget.javascriptChannels,
-              withJavascript: widget.withJavascript,
-              clearCache: widget.clearCache,
-              clearCookies: widget.clearCookies,
-              hidden: widget.hidden,
-              enableAppScheme: widget.enableAppScheme,
-              userAgent: widget.userAgent,
-              rect: _rect,
-              withZoom: widget.withZoom,
-              displayZoomControls: widget.displayZoomControls,
-              withLocalStorage: widget.withLocalStorage,
-              withLocalUrl: widget.withLocalUrl,
-              localUrlScope: widget.localUrlScope,
-              withOverviewMode: widget.withOverviewMode,
-              useWideViewPort: widget.useWideViewPort,
-              scrollBar: widget.scrollBar,
-              supportMultipleWindows: widget.supportMultipleWindows,
-              appCacheEnabled: widget.appCacheEnabled,
-              allowFileURLs: widget.allowFileURLs,
-              invalidUrlRegex: widget.invalidUrlRegex,
-              geolocationEnabled: widget.geolocationEnabled,
-              debuggingEnabled: widget.debuggingEnabled,
-            );
-          } else {
-            if (_rect != value) {
-              _rect = value;
-              _resizeTimer?.cancel();
-              _resizeTimer = Timer(const Duration(milliseconds: 250), () {
-                // avoid resizing to fast when build is called multiple time
-                webviewReference.resize(_rect);
-              });
-            }
-          }
-        },
-        child: widget.initialChild ??
-            const Center(child: const CircularProgressIndicator()),
-      ),
-    );
-  }
-}
-
-class _WebviewPlaceholder extends SingleChildRenderObjectWidget {
-  const _WebviewPlaceholder({
-    Key key,
-    @required this.onRectChanged,
-    Widget child,
-  }) : super(key: key, child: child);
-
-  final ValueChanged<Rect> onRectChanged;
-
-  @override
-  RenderObject createRenderObject(BuildContext context) {
-    return _WebviewPlaceholderRender(
-      onRectChanged: onRectChanged,
-    );
-  }
-
-  @override
-  void updateRenderObject(
-      BuildContext context, _WebviewPlaceholderRender renderObject) {
-    renderObject..onRectChanged = onRectChanged;
-  }
-}
-
-class _WebviewPlaceholderRender extends RenderProxyBox {
-  _WebviewPlaceholderRender({
-    RenderBox child,
-    ValueChanged<Rect> onRectChanged,
-  })  : _callback = onRectChanged,
-        super(child);
-
-  ValueChanged<Rect> _callback;
-  Rect _rect;
-
-  Rect get rect => _rect;
-
-  set onRectChanged(ValueChanged<Rect> callback) {
-    if (callback != _callback) {
-      _callback = callback;
-      notifyRect();
-    }
-  }
-
-  void notifyRect() {
-    if (_callback != null && _rect != null) {
-      _callback(_rect);
-    }
-  }
-
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    super.paint(context, offset);
-    final rect = offset & size;
-    if (_rect != rect) {
-      _rect = rect;
-      notifyRect();
-    }
+        appBar: appBar,
+        resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+        persistentFooterButtons: persistentFooterButtons,
+        bottomNavigationBar: bottomNavigationBar,
+        body: WebviewRect(
+          url: url,
+          headers: headers,
+          javascriptChannels: javascriptChannels,
+          withJavascript: withJavascript,
+          clearCache: clearCache,
+          clearCookies: clearCookies,
+          hidden: hidden,
+          enableAppScheme: enableAppScheme,
+          userAgent: userAgent,
+          withZoom: withZoom,
+          displayZoomControls: displayZoomControls,
+          withLocalStorage: withLocalStorage,
+          withLocalUrl: withLocalUrl,
+          localUrlScope: localUrlScope,
+          withOverviewMode: withOverviewMode,
+          useWideViewPort: useWideViewPort,
+          scrollBar: scrollBar,
+          supportMultipleWindows: supportMultipleWindows,
+          appCacheEnabled: appCacheEnabled,
+          allowFileURLs: allowFileURLs,
+          invalidUrlRegex: invalidUrlRegex,
+          geolocationEnabled: geolocationEnabled,
+          debuggingEnabled: debuggingEnabled,
+          initialChild: initialChild,
+        ));
   }
 }
