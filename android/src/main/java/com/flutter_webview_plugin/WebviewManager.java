ceca9513e450a362946348fedac7a498f72ff957
tmulin <tmulin@qq.com>
2019-11-27 15:11:22 +0800

    Revert "Fix audio play not stop after view disposed problem at Android platform."
    This reverts commit b808f6261ce46d39c2f3a4c737f00602cf5680c9.

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -7,7 +7,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Handler;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -439,8 +438,6 @@ class WebviewManager {
         if (webView != null) {
             ViewGroup vg = (ViewGroup) (webView.getParent());
             vg.removeView(webView);
-            webView.onPause();
-            webView.destroy();;
         }
         webView = null;
         if (result != null) {
