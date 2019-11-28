e411ebc826a7cf4a2a1cd84f86b5da3634615838
Naveen Gattu <naveen.gattu@gmail.com>
2019-11-28 11:13:55 -0800

    Update WebviewManager.java

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -28,6 +28,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.io.File;
 import java.util.Date;
 import java.io.IOException;
