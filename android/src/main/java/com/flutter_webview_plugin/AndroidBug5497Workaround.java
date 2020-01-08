75ab2d98d1733ee2dfb131101e57a98c801c3886
张健夫 <451458765@qq.com>
2020-01-08 15:03:43 +0800

    fix: 修改全屏H5动态高度的返回值

--- android/src/main/java/com/flutter_webview_plugin/AndroidBug5497Workaround.java
+++ android/src/main/java/com/flutter_webview_plugin/AndroidBug5497Workaround.java
@@ -11,12 +11,14 @@ import android.widget.FrameLayout;
  * Author: Jack Zhang
  * create on: 2020-01-08 14:16
  */
-public class AndroidBug5497Workaround {
+public class AndroidBug5497Workaround
+{
 
   // For more information, see https://code.google.com/p/android/issues/detail?id=5497
   // To use this class, simply invoke assistActivity() on an Activity that already has its content view set.
 
-  public static void assistActivity(Activity activity) {
+  public static void assistActivity(Activity activity)
+  {
     new AndroidBug5497Workaround(activity);
   }
 
@@ -24,32 +26,39 @@ public class AndroidBug5497Workaround {
   private int usableHeightPrevious;
   private FrameLayout.LayoutParams frameLayoutParams;
 
-  private AndroidBug5497Workaround(Activity activity) {
+  private AndroidBug5497Workaround(Activity activity)
+  {
     //Decorview里分为title和content，content即是承载我们setContentView方法的布局的根布局
     FrameLayout content = (FrameLayout) activity.findViewById(android.R.id.content);
     //mChildOfContent我们setContentView方法的布局
     mChildOfContent = content.getChildAt(0);
     //监听布局变化，任何界面变化都会触发该监听
     //软键盘弹起同样也会触发该监听
-    mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-      public void onGlobalLayout() {
+    mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
+    {
+      public void onGlobalLayout()
+      {
         possiblyResizeChildOfContent();
       }
     });
     frameLayoutParams = (FrameLayout.LayoutParams) mChildOfContent.getLayoutParams();
   }
 
-  private void possiblyResizeChildOfContent() {
+  private void possiblyResizeChildOfContent()
+  {
     int usableHeightNow = computeUsableHeight();
-    if (usableHeightNow != usableHeightPrevious) {
+    if (usableHeightNow != usableHeightPrevious)
+    {
       int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
       //计算布局变化的高度
       int heightDifference = usableHeightSansKeyboard - usableHeightNow;
-      if (heightDifference > (usableHeightSansKeyboard / 4)) {
+      if (heightDifference > (usableHeightSansKeyboard / 4))
+      {
         // keyboard probably just became visible
         //如果布局变化的高度大于全屏高度的4分之一，则认为可能是键盘弹出，需要改变我们setContentView的布局高度
         frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
-      } else {
+      } else
+      {
         // keyboard probably just became hidden
         frameLayoutParams.height = usableHeightSansKeyboard;
       }
@@ -60,11 +69,13 @@ public class AndroidBug5497Workaround {
   }
 
   //计算去掉键盘高度后的可用高度
-  private int computeUsableHeight() {
+  private int computeUsableHeight()
+  {
     Rect r = new Rect();
     mChildOfContent.getWindowVisibleDisplayFrame(r);
-    return (r.bottom - r.top);// 全屏模式下： return r.bottom
-//        return r.bottom;
+//    return (r.bottom - r.top);
+    // 全屏模式下： return r.bottom
+    return r.bottom;
   }
 
 }
