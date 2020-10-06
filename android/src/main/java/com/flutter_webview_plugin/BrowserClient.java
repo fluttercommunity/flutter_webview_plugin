3860b2d2f7279d38bcd2ef6237e152da9c3a1048
shinriyo <shinriyo@gmail.com>
2020-10-06 16:43:15 +0900

    I mistook import
    import android.webkit.WebResourceError;

--- android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
+++ android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
@@ -7,6 +7,7 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebResourceError;
 
 import java.util.HashMap;
 import java.util.Map;
