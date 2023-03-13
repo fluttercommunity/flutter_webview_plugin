e301b2e1e4d7152d58fc96dbea1c821db766211d
Antwen <55020131+Antwen97@users.noreply.github.com>
2023-03-13 12:29:22 +0100

    Migration to V2 embedding

--- android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
+++ android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
@@ -16,6 +16,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -24,7 +28,7 @@ import io.flutter.plugin.common.PluginRegistry;
 /**
  * FlutterWebviewPlugin
  */
-public class FlutterWebviewPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
+public class FlutterWebviewPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler, PluginRegistry.ActivityResultListener {
     private Activity activity;
     private WebviewManager webViewManager;
     private Context context;
@@ -40,6 +44,8 @@ public class FlutterWebviewPlugin implements MethodCallHandler, PluginRegistry.A
             channel.setMethodCallHandler(instance);
         }
     }
+
+    public FlutterWebviewPlugin() {}
 
     FlutterWebviewPlugin(Activity activity, Context context) {
         this.activity = activity;
@@ -324,4 +330,34 @@ public class FlutterWebviewPlugin implements MethodCallHandler, PluginRegistry.A
         }
         return false;
     }
+
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL_NAME);
+        context = binding.getApplicationContext();
+
+        channel.setMethodCallHandler(this);
+
+        //final FlutterWebviewPlugin instance = new FlutterWebviewPlugin(registrar.activity(), registrar.activeContext());
+        //registrar.addActivityResultListener(instance);
+
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {}
+
+    @Override
+    public void onAttachedToActivity(ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        binding.addActivityResultListener(this);
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {}
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {}
+
+    @Override
+    public void onDetachedFromActivity() {}
 }
