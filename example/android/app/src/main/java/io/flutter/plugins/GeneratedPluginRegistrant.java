1359ecb6386d1d49dc265897234193c071d87a33
hardy716 <iabc716@icloud.com>
2023-05-18 15:29:06 +0900

    fix:Replace deprecated setAppCacheEnabled with setCacheMode

--- example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,35 +1,25 @@
 package io.flutter.plugins;
 
-// import io.flutter.plugin.common.PluginRegistry;
-import androidx.annotation.NonNull;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
+import io.flutter.plugin.common.PluginRegistry;
 import com.flutter_webview_plugin.FlutterWebviewPlugin;
 
 /**
  * Generated file. Do not edit.
  */
-// public final class GeneratedPluginRegistrant {
-//   public static void registerWith(PluginRegistry registry) {
-//     if (alreadyRegisteredWith(registry)) {
-//       return;
-//     }
-//     FlutterWebviewPlugin.registerWith(registry.registrarFor("com.flutter_webview_plugin.FlutterWebviewPlugin"));
-//   }
-
-//   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
-//     final String key = GeneratedPluginRegistrant.class.getCanonicalName();
-//     if (registry.hasPlugin(key)) {
-//       return true;
-//     }
-//     registry.registrarFor(key);
-//     return false;
-//   }
-// }
-
 public final class GeneratedPluginRegistrant {
-  public static void registerWith(@NonNull FlutterEngine flutterEngine) {
-      ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
-      FlutterWebviewPlugin.registerWith(shimPluginRegistry.registrarFor("com.flutter_webview_plugin.FlutterWebviewPlugin"));
+  public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
+    FlutterWebviewPlugin.registerWith(registry.registrarFor("com.flutter_webview_plugin.FlutterWebviewPlugin"));
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
   }
 }
